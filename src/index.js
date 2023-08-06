053f2103db64cd1acbbaade9228a625d52c04b51
Iyadi Cyril <iyadicy@gmail.com>
2023-08-06 16:32:30 +0100

    add service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './mainPage';
 import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './reportServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+registerServiceWorker();
