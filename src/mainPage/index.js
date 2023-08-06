740117612a213b69f89d076d27e7085725c971e2
Iyadi Cyril <iyadicy@gmail.com>
2023-08-06 02:13:01 +0100

    new routes for search country and selected houses

--- src/mainPage/index.js
+++ src/mainPage/index.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './mainPage.css';
 import FeaturedHouse from './featuredHouse';
+import SearchResults from '../searchResults';
+import HouseFromQuery from '../house/houseFromQuery';
+import HouseFilter from './houseFilter';
 import React, { useEffect, useState, useMemo } from 'react';
 import Header from './header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -29,10 +32,12 @@ function App() {
     <Router>
       <div className="container">
         <Header subtitle='Providing housing all over the world' />
+        <HouseFilter allHouses={allHouses} />
 
         <Routes>
-          <Route path="/" element={ <FeaturedHouse house={featuredHouse} /> } >
-          </Route>
+          <Route exact path="/" element={ <FeaturedHouse house={featuredHouse} /> } />
+          <Route path="/searchResults/:country" element={ <SearchResults allHouses={allHouses} /> } />
+          <Route path="/house/:id" element={ <HouseFromQuery allHouses={allHouses} /> } />
         </Routes>
       </div>
     </Router>
