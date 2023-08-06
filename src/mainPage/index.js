b20f57dc5f2d259ef8a1560806c02fc7a7bafa58
Iyadi Cyril <iyadicy@gmail.com>
2023-08-06 01:22:00 +0100

    add routing

--- src/mainPage/index.js
+++ src/mainPage/index.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './mainPage.css';
+import FeaturedHouse from './featuredHouse';
 import React, { useEffect, useState, useMemo } from 'react';
 import Header from './header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [ allHouses, setAllHouses ] = useState([])
@@ -10,7 +12,7 @@ function App() {
     const fetchHouses = async () => {
       const response = await fetch('/houses.json');
       const houses = await response.json();
-      setAllHouses(allHouses);
+      setAllHouses(houses);
     }
 
     fetchHouses();
@@ -18,15 +20,23 @@ function App() {
 
   const featuredHouse = useMemo(() => {
     if(allHouses.length){
-      const index = Math.floor(allHouses.lenght * Math.random())
+      const index = Math.floor(allHouses.length * Math.random())
       return allHouses[index]
     }
   },[allHouses]);
 
   return (
-    <div className="container">
-      <Header subtitle='Providing housing all over the world' />
-    </div>
+    <Router>
+      <div className="container">
+        <Header subtitle='Providing housing all over the world' />
+
+        <Routes>
+          <Route path="/" element={ <FeaturedHouse house={featuredHouse} /> } >
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
