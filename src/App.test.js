7cc4011e8203617c00638d8af119d4a206d9721d
Iyadi Cyril <iyadicy@gmail.com>
2023-08-06 16:32:14 +0100

    fix failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './mainPage';
+import { unmountComponentAtNode } from 'react-dom';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+  const div = document.createElement('div')
+  render(<App />, div);
+  unmountComponentAtNode(div);
 });
